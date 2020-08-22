d550b7282d9e5d941d1096f5b1f2dbf6d35826e3
Mohammed Abdul Jawad Vajeeh <51875089+jawadsab@users.noreply.github.com>
2020-08-22 13:40:55 +0530

    name route

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flaskapp import app
 def home():
     return "<h1>Hello World</h1>"
 
+@app.route("/<name>")
+def home_user(name):
+    return f"<h1>Hello {name}</h1>"
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
